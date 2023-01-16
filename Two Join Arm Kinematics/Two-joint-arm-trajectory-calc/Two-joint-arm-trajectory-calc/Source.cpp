70f41ae8f62b507cbc6dbfbda3f6d688a4bc4049
Alex Chang <alex.chang@team114.org>
2023-01-16 15:00:29 -0800

    addded README and TwoJointArmProfiles

--- Two Join Arm Kinematics/Two-joint-arm-trajectory-calc/Two-joint-arm-trajectory-calc/Source.cpp
+++ Two Join Arm Kinematics/Two-joint-arm-trajectory-calc/Two-joint-arm-trajectory-calc/Source.cpp
@@ -1,5 +1,6 @@
 #include "armKinematics.h"
 #include "TaskSpaceTrajectoryCalc.h"
+#include "TwoJointArmProfiles.h"
 #include <iostream>
 
 using namespace std;
@@ -29,6 +30,36 @@ int main()
 	TaskSpaceTrajectoryCalc calc{ TwoJointArmConstants::LOWER_ARM_MAX_VEL, TwoJointArmConstants::LOWER_ARM_MAX_ACC, TwoJointArmConstants::UPPER_ARM_MAX_VEL, TwoJointArmConstants::UPPER_ARM_MAX_ACC,
 	TwoJointArmConstants::LOWER_ARM_LENGTH, TwoJointArmConstants::UPPER_ARM_LENGTH, 0.5, 0.25 };
 
-	calc.generateLinearTrajectory("yeah.csv", -8, 166, 1.3208, 0.5842);
+	//Stowed,		 0.221, 0.26247, 18.84, 160.11
+	//Ground intake, 0.40564, 0.53042, 59.59, 129.87
+	//player station 0.54164, 0.43525, -29.64, 127.4
+	//mid,			 1.02512, 0.29191, 20, 90
+	//high,			 1.38078, 0.54399, 59.18, 16.16
+
+	/*calc.generateLinearTrajectory("01.csv", 18.84, 160.11, 0.40564, 0.53042);
+	calc.generateLinearTrajectory("02.csv", 18.84, 160.11, 0.54164, 0.43525);
+	calc.generateLinearTrajectory("03.csv", 18.84, 160.11, 1.02512, 0.29191);
+	calc.generateLinearTrajectory("04.csv", 18.84, 160.11, 1.38078, 0.54399);
+	calc.generateLinearTrajectory("10.csv", 59.59, 129.87, 0.221, 0.26247);
+	calc.generateLinearTrajectory("12.csv", 59.59, 129.87, 0.54164, 0.43525);
+	calc.generateLinearTrajectory("13.csv", 59.59, 129.87, 1.02512, 0.29191);
+	calc.generateLinearTrajectory("14.csv", 59.59, 129.87, 1.38078, 0.54399);
+	calc.generateLinearTrajectory("20.csv", -29.64, 127.4, 0.221, 0.26247);
+	calc.generateLinearTrajectory("21.csv", -29.64, 127.4, 0.40564, 0.53042);
+	calc.generateLinearTrajectory("23.csv", -29.64, 127.4, 1.02512, 0.29191);
+	calc.generateLinearTrajectory("24.csv", -29.64, 127.4, 1.38078, 0.54399);
+	calc.generateLinearTrajectory("30.csv", 20, 90, 0.221, 0.26247);
+	calc.generateLinearTrajectory("31.csv", 20, 90, 0.40564, 0.53042);
+	calc.generateLinearTrajectory("32.csv", 20, 90, 0.54164, 0.43525);
+	calc.generateLinearTrajectory("34.csv", 20, 90, 1.38078, 0.54399);
+	calc.generateLinearTrajectory("40.csv", 59.18, 16.16, 0.221, 0.26247);
+	calc.generateLinearTrajectory("41.csv", 59.18, 16.16, 0.40564, 0.53042);
+	calc.generateLinearTrajectory("42.csv", 59.18, 16.16, 0.54164, 0.43525);
+	calc.generateLinearTrajectory("43.csv", 59.18, 16.16, 1.02512, 0.29191);*/
+
+
+
+	TwoJointArmProfiles profiles;
+	profiles.readProfiles();
 
 }
--- Two Join Arm Kinematics/Two-joint-arm-trajectory-calc/Two-joint-arm-trajectory-calc/TwoJointArmProfiles.cpp
+++ Two Join Arm Kinematics/Two-joint-arm-trajectory-calc/Two-joint-arm-trajectory-calc/TwoJointArmProfiles.cpp
@@ -7,30 +7,33 @@ TwoJointArmProfiles::TwoJointArmProfiles()
 
 void TwoJointArmProfiles::readProfiles()
 {
-	for(int i = STOWED; i < HIGH; ++i)
+    if (hasProfiles_)
+    {
+        return;
+    }
+
+    int k;
+	for(int i = STOWED; i <= HIGH; ++i)
 	{
-		for(int j = STOWED; i < HIGH; ++i)
+        k = 0;
+		for(int j = STOWED; j <= HIGH; ++j)
 		{
 			if(i == j)
 			{
-				break;
+				continue;
 			}
 
 			pair<Positions, Positions> key{static_cast<Positions>(i), static_cast<Positions>(j)};
 			map<double, pair<tuple<double, double, double>, tuple<double, double, double>>> profile;
             string fileName = to_string(i) + to_string(j) + ".csv";
 
-            if (hasProfiles_)
-            {
-                return;
-            }
             ifstream infile(fileName);
 
             string data;
-            double distance, angle, velocity, partDer;
+            double time, omegaPos, omegaVel, omegaAcc, phiPos, phiVel, phiAcc;
 
             bool valid;
-            std::size_t c1, c2, c3;
+            std::size_t c1, c2, c3, c4, c5, c6;
 
             while (getline(infile, data))
             {
@@ -39,18 +42,48 @@ void TwoJointArmProfiles::readProfiles()
                 c1 = data.find(", ");
                 if (c1 != string::npos)
                 {
-                    distance = stod(data.substr(0, c1));
+                    time = stod(data.substr(0, c1));
 
                     c2 = data.find(", ", c1 + 1);
                     if (c2 != string::npos)
                     {
-                        angle = stod(data.substr(c1 + 2, c2));
+                        omegaPos = stod(data.substr(c1 + 2, c2));
 
                         c3 = data.find(", ", c2 + 1);
                         if (c3 != string::npos)
                         {
-                            velocity = stod(data.substr(c2 + 2, c3));
-                            partDer = stod(data.substr(c3 + 2));
+                            omegaVel = stod(data.substr(c2 + 2, c3));
+
+                            c4 = data.find(", ", c3 + 1);
+                            if (c4 != string::npos)
+                            {
+                                omegaAcc = stod(data.substr(c3 + 2, c4));
+
+                                c5 = data.find(", ", c4 + 1);
+                                if (c5 != string::npos)
+                                {
+                                    phiPos = stod(data.substr(c4 + 2, c5));
+
+                                    c6 = data.find(", ", c5 + 1);
+                                    if (c6 != string::npos)
+                                    {
+                                        phiVel = stod(data.substr(c5 + 2, c6));
+                                        phiAcc = stod(data.substr(c6 + 2));
+                                    }
+                                    else
+                                    {
+                                        valid = false;
+                                    }
+                                }
+                                else
+                                {
+                                    valid = false;
+                                }
+                            }
+                            else
+                            {
+                                valid = false;
+                            }
                         }
                         else
                         {
@@ -70,17 +103,28 @@ void TwoJointArmProfiles::readProfiles()
 
                 if (valid)
                 {
-                    tuple<double, double, double> shotData(angle, velocity, partDer);
-                    pair<double, tuple<double, double, double>> distancePoint(distance, shotData);
+                    tuple<double, double, double> omega{omegaPos, omegaVel, omegaAcc};
+                    tuple<double, double, double> phi{phiPos, phiVel, phiAcc};
+                    pair<tuple<double, double, double>, tuple<double, double, double>> fullAngPose{omega, phi};
+                    pair<double, pair<tuple<double, double, double>, tuple<double, double, double>>> angPoint{time, fullAngPose};
 
-                    profile.insert(distancePoint);
+                    profile.insert(angPoint);
+
+                    ++k;
                 }
             }
+            
+            pair<pair<Positions, Positions>, map<double, pair<tuple<double, double, double>, tuple<double, double, double>>>> positionMapPoint{ key, profile };
+            profiles_.insert(positionMapPoint);
+            cout << "inserted " << k << " points" << endl;
 
-            //hasMap_ = true;
-            //frc::SmartDashboard::PutNumber("Map Points", mapPoints_);
+            pair<Positions, Positions> testKey{ STOWED, HIGH };
+            //cout << get<1>(profiles_.at(key).upper_bound(0.1)->second.second) << endl;
+            //0.1001, -8.09238, 165.943, -2.04329, -1.03383, -20.4125, -10.328
             infile.close();
-
 		}
+
 	}
+
+    hasProfiles_ = true;
 }
